f194dffd37d95f67bdea981e8587e345b17e01bd
cybersecurity-1 <112893543+cybersecurity-1@users.noreply.github.com>
2024-01-03 11:40:08 +0530

    Add files via upload

---
+++ coperate.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var x = document.getElementById("demo-1");
+    if (x.className === "bi-chevron-down") {
+      x.className = "bi-chevron-up";
+    } else {
+      x.className = "bi-chevron-down";
+    }
+  }
+  
+  function Function1() {
+    var x = document.getElementById("demo-2");
+    if (x.className === "bi-chevron-down") {
+      x.className = "bi-chevron-up";
+    } else {
+      x.className = "bi-chevron-down";
+    }
+  }
+
+  function Function2() {
+    var x = document.getElementById("demo-3");
+    if (x.className === "bi-chevron-down") {
+      x.className = "bi-chevron-up";
+    } else {
+      x.className = "bi-chevron-down";
+    }
+  }
